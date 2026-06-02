b7e1bead0b5fb0948d0682b53ba40b586620166e
Nora Strong <gblues@gmail.com>
2026-06-01 18:06:51 -0700

    remove stuff from my custom config

--- src/gblues/main/main.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("asm/gblues/nonmatchings/main/main", func_801C55C0);
--- src/gblues/main/misc.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("asm/gblues/nonmatchings/main/misc", func_801C7F10);
-
-INCLUDE_ASM("asm/gblues/nonmatchings/main/misc", func_801C80B4);
