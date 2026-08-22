c13c2ad86002a96b4405472b3a4346679c8018ff
Nora Strong <gblues@gmail.com>
2026-08-22 08:59:01 -0700

    add a match

--- src/gear_shop_menu/main/main.c
+++ src/gear_shop_menu/main/main.c
@@ -75,6 +75,8 @@ extern u32 D_801D6A80;
 extern int D_801D6A84[];
 extern int D_801D6AFC[];
 extern int D_801D6B7C[];
+extern u32 D_801D6BFC[9]; // = {0x49, 0x48, 0x43, 0x3C, 0x25, 0x24, 0x1F, 0x18, 0x10}
+extern s32 D_801D6C20[9]; // = {0xC9, 0xB5, 0xA2, 0x90, 0xC9, 0xB5, 0xA2, 0x90, 0x80}
 extern int D_801D6C44[];
 extern u16 D_801D6C68[];
 extern int g_gearShopGearEquipFlags[];
@@ -2092,15 +2094,56 @@ void func_801CCE90(s32 count, MenuString *menuString, s8 *arg2, s8 *unused) {
     func_801C5EE8(menuString, arg2, 2, (u8)count);
 }
 
-void func_801CCEBC(s32 count, s8* buffer) {
+void func_801CCEBC(u8 count, s8* buffer) {
     int i;
 
-    for(i = 0; i < (u8)count; i++) {
+    for(i = 0; i < count; i++) {
         buffer[i] = 0;
     }
 }
 
-INCLUDE_ASM("asm/gear_shop_menu/nonmatchings/main/main", func_801CCEE8);
+void func_801CCEE8(s32 arg0, MenuString *pStringList, s32 __unused, int* pPaddingOffsets, u8 *pUnkByteArray, u8 choice, u8 scrollOffset, u8 unkBoolean) {
+    switch(unkBoolean) {
+        case FALSE:
+            func_801CCEBC(arg0, pUnkByteArray);
+
+            setXY4(&pStringList[choice].polys[g_Menu->renderContext],
+                (D_801D6BFC[scrollOffset + choice]) + (22 + pPaddingOffsets[choice]), // x0
+                D_801D6C20[scrollOffset + choice] - 34, // y0
+
+                pStringList[choice].width + ((D_801D6BFC[scrollOffset + choice]) + (22 + pPaddingOffsets[choice])), // x1
+                D_801D6C20[scrollOffset + choice] - 34, // y1
+
+                (D_801D6BFC[scrollOffset + choice]) + (22 + pPaddingOffsets[choice]), // x2
+                D_801D6C20[scrollOffset + choice] - 21, // y2
+
+                pStringList[choice].width + ((D_801D6BFC[scrollOffset + choice]) + (22 + pPaddingOffsets[choice])), // x3
+                D_801D6C20[scrollOffset + choice] - 21 // y3
+            );
+            break;
+        case TRUE:
+
+            if(choice) {
+                setXY4(&pStringList[choice].polys[g_Menu->renderContext],
+                       206, 126,
+                       pStringList[0].width + 206, 126,
+                       206, 139,
+                       pStringList[0].width + 206, 139
+                );
+            } else {
+                setXY4(&pStringList[choice].polys[g_Menu->renderContext],
+                       236, 126,
+                       pStringList[choice].width + 236, 126,
+                       236, 139,
+                       pStringList[choice].width + 236, 139
+                );
+            }
+            break;
+
+    }
+    pStringList[choice].renderContext = g_Menu->renderContext;
+    pUnkByteArray[choice] = 1;
+}
 
 void GearShopMenuInitializeShopModeSelectionMenu(int count, int* pResourceIDs) {
     int i, j;
